6043402d6acd06a2f26e45d06c2719f0c516011f
Micha de Vries <micha@devrie.sh>
2022-11-23 13:31:49 +0100

    Update next config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ["@svgr/webpack"]
-  //   });
-
-  //   return config;
-  // }
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig

2c947390771f54fb52fa6cf6a14de3a2c2ba681a
Hesselaar23 <81434106+Hesselaar23@users.noreply.github.com>
2022-11-28 21:53:25 +0100

    fixed fail

--- pages/get-started.tsx
+++ pages/get-started.tsx
@@ -4,19 +4,15 @@ import { Input } from '../components/Input';
 import styles from '../styles/pages/GetStarted.module.scss';
 
 export default function GetStarted() {
-  return (
-    <div className={styles.default}>   
-            
-        <h1>Get started</h1>
-        
-        <Input type="name" placeholder='Name of organisation'/> 
-        
-        <Input type="email" placeholder='E-mail adres'/>    
+    return (
+        <div className={styles.default}>
+            <h1>Get started</h1>
 
-        <Button href='/'>
-            confirm my email
-        </Button>
+            <Input type="name" placeholder="Name of organisation" />
 
-    </div>
-  )
+            <Input type="email" placeholder="E-mail adres" />
+
+            <Button href="/">confirm my email</Button>
+        </div>
+    );
 }

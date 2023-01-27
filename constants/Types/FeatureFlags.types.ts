bcc2b95b9485abbf3e1de742644f846cab7ea007
Micha de Vries <micha@devrie.sh>
2023-01-27 22:18:14 +0100

    Linting

--- constants/Types/FeatureFlags.types.ts
+++ constants/Types/FeatureFlags.types.ts
@@ -1,6 +1,6 @@
 export type TEnv = 'prod' | 'dev';
 export const FeatureFlagOptions = ['preLaunchPage', 'devTools'] as const;
-export type TFeatureFlagOptions = typeof FeatureFlagOptions[number];
+export type TFeatureFlagOptions = (typeof FeatureFlagOptions)[number];
 
 export type TFeatureFlags = {
     [key in TFeatureFlagOptions]: boolean;

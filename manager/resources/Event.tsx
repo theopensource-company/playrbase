ad67e5ef086b4f4dfcead1f5512ad78152e00808
Micha de Vries <micha@devrie.sh>
2023-01-28 10:44:13 +0100

    Improve event management

--- manager/resources/Event.tsx
+++ manager/resources/Event.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 import {
     List,
     Datagrid,
@@ -24,20 +24,77 @@ import {
     DateTimeInput,
     FunctionField,
     SelectInput,
+    CloneButton,
+    TopToolbar,
+    SelectColumnsButton,
+    DatagridConfigurable,
+    SortButton,
+    CreateButton,
+    useGetIdentity,
+    useGetOne,
+    ShowButton,
 } from 'react-admin';
 import { TableHead, TableRow, TableCell } from '@mui/material';
 import { RichTextInput } from 'ra-input-rich-text';
 import {
     EventCategories,
     TEventCategories,
+    TEventRecord,
 } from '../../constants/Types/Events.types';
+import { TOrganisationRecord } from '../../constants/Types/Organisation.types';
+
+const EventListActions = () => (
+    <TopToolbar>
+        <SortButton fields={['created', 'updated', 'start', 'end']} />
+        <SelectColumnsButton />
+        <CreateButton />
+    </TopToolbar>
+);
+
+const eventListFilters = [
+    <ReferenceInput
+        label="Organiser"
+        key="org"
+        source="organiser"
+        reference="organisation"
+    />,
+];
+
+const CanWriteOrganisation = ({
+    children,
+    fallback,
+}: {
+    children: ReactNode;
+    fallback?: ReactNode;
+}) => {
+    const ctx = useRecordContext<TEventRecord>();
+    const { data: identity, isLoading: identityLoading } = useGetIdentity();
+    const { data: organisation, isLoading: organisationLoading } =
+        useGetOne<TOrganisationRecord>('organisation', { id: ctx.organiser });
+    if (identityLoading || organisationLoading)
+        return fallback ? <>{fallback}</> : null;
+    if (
+        organisation?.manager_roles.find(
+            (r) => r.id == identity?.id && r.role !== 'event_viewer'
+        )
+    ) {
+        return <>{children}</>;
+    }
+
+    return fallback ? <>{fallback}</> : null;
+};
 
 export const EventList = () => (
-    <List sort={{ field: 'created', order: 'DESC' }}>
-        <Datagrid rowClick="show">
+    <List
+        sort={{ field: 'created', order: 'DESC' }}
+        actions={<EventListActions />}
+        filters={eventListFilters}
+    >
+        <DatagridConfigurable rowClick="show">
             <TextField source="id" />
             <TextField source="name" />
             <FunctionField
+                label="Category"
                 render={({ category }: { category: TEventCategories }) =>
                     EventCategories[category]
                 }
@@ -56,8 +113,11 @@ export const EventList = () => (
             </ReferenceField>
             <DateField source="created" />
             <DateField source="updated" />
-            <EditButton />
-        </Datagrid>
+            <CloneButton />
+            <CanWriteOrganisation fallback={<ShowButton />}>
+                <EditButton />
+            </CanWriteOrganisation>
+        </DatagridConfigurable>
     </List>
 );
 
@@ -66,9 +126,24 @@ const EventTitle = () => {
     return <>{ctx ? `Event: ${ctx.name}` : 'Loading'}</>;
 };
 
+const ShowEventActions = () => (
+    <div
+        style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            paddingTop: '30px',
+            paddingBottom: '5px',
+        }}
+    >
+        <CanWriteOrganisation fallback={<CloneButton />}>
+            <EditButton />
+        </CanWriteOrganisation>
+    </div>
+);
+
 export const ShowEvent = () => {
     return (
-        <Show title={<EventTitle />}>
+        <Show title={<EventTitle />} actions={<ShowEventActions />}>
             <TabbedShowLayout>
                 <Tab label="details">
                     <TextField source="id" />
@@ -153,11 +228,7 @@ export const EditEvent = () => (
             <RichTextInput source="description" />
             <BooleanInput source="discoverable" />
             <BooleanInput source="published" />
-            <ReferenceInput
-                source="tournament"
-                reference="event"
-                emptyValue=""
-            />
+            <ReferenceInput source="tournament" reference="event" />
             <DateTimeInput source="start" />
             <DateTimeInput source="end" />
         </SimpleForm>
@@ -188,7 +259,6 @@ export const CreateEvent = () => (
                 source="tournament"
                 reference="event"
                 emptyText="Part of tournament"
-                emptyValue=""
             />
             <DateTimeInput source="start" />
             <DateTimeInput source="end" />

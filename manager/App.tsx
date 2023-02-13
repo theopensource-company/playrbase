33abd5b473e54aa7f0b40de9564fda83fd6e547b
Micha de Vries <micha@devrie.sh>
2023-02-13 17:03:04 +0100

    Change manager login background

--- manager/App.tsx
+++ manager/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Login } from 'react-admin';
 import authProvider from './Auth';
 import Fetcher from './Fetcher';
 import { EventResource } from './resources/Event';
 import { OrganisationResource } from './resources/Organisation';
 
+const LoginPage = () => <Login backgroundImage="/bgnoise-1080.png" />;
 const App = () => (
-    <Admin dataProvider={Fetcher()} authProvider={authProvider}>
+    <Admin
+        dataProvider={Fetcher()}
+        authProvider={authProvider}
+        loginPage={LoginPage}
+    >
         {OrganisationResource}
         {EventResource}
     </Admin>

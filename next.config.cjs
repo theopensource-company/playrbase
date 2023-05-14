3986583b9f5d1782a7c27e082508a4a080b019bf
Micha de Vries <micha@devrie.sh>
2023-05-14 12:41:22 +0200

    Add i18n config to next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -5,6 +5,10 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
 };
 
 module.exports = nextConfig;

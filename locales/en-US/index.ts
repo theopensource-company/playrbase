a99be5cba1801228316b8397c56ced68b4ee6d73
Micha de Vries <micha@devrie.sh>
2023-06-04 17:57:45 +0200

    Upgrade packages

--- locales/en-US/index.ts
+++ locales/en-US/index.ts
@@ -3,4 +3,4 @@ import components from './components.json';
 import constants from './constants.json';
 import pages from './pages.json';
 
-export { pages, common, components, constants };
+export { common, components, constants, pages };

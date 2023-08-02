1f59abd576416658c2b37aa3f2b6c515528d9f54
Micha de Vries <micha@devrie.sh>
2023-08-02 11:20:16 +0200

    Improve email devtool

--- src/components/layout/DevTools/emails.tsx
+++ src/components/layout/DevTools/emails.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Skeleton } from '@/components/ui/skeleton';
 import {
     Table,
     TableBody,
@@ -12,20 +13,36 @@ import {
     TableRow,
 } from '@/components/ui/table';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { Email } from '@/constants/Types/Email.types';
+import { TooltipProvider } from '@radix-ui/react-tooltip';
+import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { useTranslations } from 'next-intl';
 import React from 'react';
-import useSWR from 'swr';
+
+dayjs.extend(duration);
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
 
 export default function Devtools_Emails() {
-    const list = async () => {
-        const raw = await fetch('/api/devkit/email/list');
-        return (await raw.json()) as {
-            success: true;
-            emails: Record<string, Email & { sent: Date }>;
-        };
-    };
-    const { data } = useSWR('devkit:/email/list', list);
+    const { data, isLoading } = useQuery({
+        queryKey: ['devkit', '/email/list'],
+        queryFn: async () => {
+            const raw = await fetch('/api/devkit/email/list');
+            return (await raw.json()) as {
+                success: true;
+                emails: Record<string, Email & { sent: Date }>;
+            };
+        },
+    });
     const t = useTranslations('components.devtools.emails');
 
     const emails = Object.entries(data?.emails ?? {})
@@ -58,60 +75,104 @@ export default function Devtools_Emails() {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {emails.map(({ key, sent, subject, to, html, text }) => (
-                        <TableRow key={key}>
-                            <TableCell>{`${sent.toDateString()}, ${sent.toLocaleTimeString()}`}</TableCell>
-                            <TableCell>{subject}</TableCell>
-                            <TableCell>{to}</TableCell>
-                            <TableCell>
-                                <Dialog>
-                                    <DialogTrigger asChild>
-                                        <Button>
-                                            {t('table.emails.actions.view')}
-                                        </Button>
-                                    </DialogTrigger>
-                                    <DialogContent
-                                        className="h-[700px] max-h-[90%] w-[800px]"
-                                        style={{ maxWidth: '90%' }}
-                                    >
-                                        <Tabs
-                                            defaultValue="html"
-                                            className="flex w-full flex-col"
-                                        >
-                                            <div>
-                                                <TabsList className="mb-4">
-                                                    <TabsTrigger value="html">
-                                                        HTML
-                                                    </TabsTrigger>
-                                                    <TabsTrigger value="text">
-                                                        Text
-                                                    </TabsTrigger>
-                                                </TabsList>
-                                            </div>
-                                            <TabsContent
-                                                value="html"
-                                                className="h-full w-full"
-                                            >
-                                                <iframe
-                                                    srcDoc={html}
-                                                    className="h-full w-full"
-                                                />
-                                            </TabsContent>
-                                            <TabsContent
-                                                value="text"
-                                                className="h-full w-full"
-                                            >
-                                                <iframe
-                                                    srcDoc={text}
-                                                    className="h-full w-full"
-                                                />
-                                            </TabsContent>
-                                        </Tabs>
-                                    </DialogContent>
-                                </Dialog>
-                            </TableCell>
-                        </TableRow>
-                    ))}
+                    {isLoading
+                        ? new Array(5).fill(0).map((_, i) => (
+                              <TableRow key={i}>
+                                  <TableCell>
+                                      <div className="flex gap-3">
+                                          <Skeleton className="h-4 w-8" />
+                                          <Skeleton className="h-4 w-8" />
+                                          <Skeleton className="h-4 w-14" />
+                                      </div>
+                                  </TableCell>
+                                  <TableCell>
+                                      <Skeleton className="h-4 w-36" />
+                                  </TableCell>
+                                  <TableCell>
+                                      <Skeleton className="h-4 w-32" />
+                                  </TableCell>
+                                  <TableCell>
+                                      <Skeleton className="h-10 w-24" />
+                                  </TableCell>
+                              </TableRow>
+                          ))
+                        : emails.map(
+                              ({ key, sent, subject, to, html, text }) => (
+                                  <TableRow key={key}>
+                                      <TableCell>
+                                          <TooltipProvider>
+                                              <Tooltip>
+                                                  <TooltipTrigger>
+                                                      {dayjs
+                                                          .duration(
+                                                              dayjs(sent).diff()
+                                                          )
+                                                          .humanize(true)}
+                                                  </TooltipTrigger>
+                                                  <TooltipContent>
+                                                      <i>
+                                                          {dayjs(sent).format(
+                                                              'LLLL'
+                                                          )}
+                                                      </i>
+                                                  </TooltipContent>
+                                              </Tooltip>
+                                          </TooltipProvider>
+                                      </TableCell>
+                                      <TableCell>{subject}</TableCell>
+                                      <TableCell>{to}</TableCell>
+                                      <TableCell>
+                                          <Dialog>
+                                              <DialogTrigger asChild>
+                                                  <Button>
+                                                      {t(
+                                                          'table.emails.actions.view'
+                                                      )}
+                                                  </Button>
+                                              </DialogTrigger>
+                                              <DialogContent
+                                                  className="h-[700px] max-h-[90%] w-[800px]"
+                                                  style={{ maxWidth: '90%' }}
+                                              >
+                                                  <Tabs
+                                                      defaultValue="html"
+                                                      className="flex w-full flex-col"
+                                                  >
+                                                      <div>
+                                                          <TabsList className="mb-4">
+                                                              <TabsTrigger value="html">
+                                                                  HTML
+                                                              </TabsTrigger>
+                                                              <TabsTrigger value="text">
+                                                                  Text
+                                                              </TabsTrigger>
+                                                          </TabsList>
+                                                      </div>
+                                                      <TabsContent
+                                                          value="html"
+                                                          className="h-full w-full"
+                                                      >
+                                                          <iframe
+                                                              srcDoc={html}
+                                                              className="h-full w-full"
+                                                          />
+                                                      </TabsContent>
+                                                      <TabsContent
+                                                          value="text"
+                                                          className="h-full w-full"
+                                                      >
+                                                          <iframe
+                                                              srcDoc={text}
+                                                              className="h-full w-full"
+                                                          />
+                                                      </TabsContent>
+                                                  </Tabs>
+                                              </DialogContent>
+                                          </Dialog>
+                                      </TableCell>
+                                  </TableRow>
+                              )
+                          )}
                 </TableBody>
             </Table>
         </div>

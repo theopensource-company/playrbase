59df0fda7c4852a25eb02ac4b73a9275df14d77b
Micha de Vries <micha@devrie.sh>
2023-10-29 10:50:45 +0100

    translate NotFoundScreen

--- src/components/layout/NotFoundScreen.tsx
+++ src/components/layout/NotFoundScreen.tsx
@@ -1,4 +1,5 @@
 import { FileSearch, LucideIcon } from 'lucide-react';
+import { useTranslations } from 'next-intl';
 import React from 'react';
 
 export function NotFoundScreen({
@@ -8,13 +9,14 @@ export function NotFoundScreen({
     icon?: LucideIcon;
     text?: string;
 }) {
+    const t = useTranslations('components.layout.not-found-screen');
     const Icon = icon ?? FileSearch;
 
     return (
         <div className="flex flex-grow items-center justify-center">
             <h1 className="flex select-none items-center gap-2 text-xl font-semibold opacity-50">
                 <Icon />
-                {text ?? 'Resource not found'}
+                {text ?? t('text')}
             </h1>
         </div>
     );
